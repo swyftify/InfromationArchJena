8d1ab9c8952007145e033bf3575290e710a11320
Pavel <tarasovp@tcd.ie>
2017-03-30 20:28:30 +0100

    Added basic SPARQL query capability to UI

--- jena/src/UIMain.java
+++ jena/src/UIMain.java
@@ -1,12 +1,43 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFNode;
 
 public class UIMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		Scanner reader = new Scanner(System.in);
 		boolean isExit = false;
 		String inputCounty = "";
+		String getRootsQuery =
+				"SELECT ?crime ?num WHERE " + "{"
+						+ "<http://lab.Jena.Kdeg.ie/Ontology1#Abbeyfeale> <http://lab.Jena.Kdeg.ie/Ontology1#hasCrime> ?crime."
+						+ "?crime <http://lab.Jena.Kdeg.ie/Ontology1#hasNumber> ?num." + "}";
+
+		// CreateModel.createModel();
+
+		OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_LITE_MEM, null);
+		readOntology("data.owl", model);
+
+		traverseStart(model, null, getRootsQuery);
+
+		// ------Default ReadModel class--------
+		// ReadModel.loadAllClassesOnt("data.owl");
 
 		while (!isExit) {
 			System.out.println("Please select one of the following options:");
@@ -96,6 +127,96 @@ public class UIMain {
 		}
 
 		return result;
+	}
+
+	public static void readOntology(String file, OntModel model) {
+		InputStream in = null;
+		try {
+			in = new FileInputStream(file);
+			model.read(in, "RDF/XML");
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static List<String> getRoots(OntModel model, String rootQuery) {
+		List<String> roots = new ArrayList<String>();
+
+		String getRootsQuery = rootQuery;
+
+		Query query = QueryFactory.create(getRootsQuery);
+
+		try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+			ResultSet results = qexec.execSelect();
+			while (results.hasNext()) {
+				QuerySolution soln = results.nextSolution();
+				RDFNode sub = soln.get("crime");
+				RDFNode sec = soln.get("num");
+
+				if (!sub.isURIResource())
+					continue;
+
+				roots.add(sub.toString());
+				roots.add(sec.toString());
+			}
+		}
+
+		return roots;
+	}
+
+	public static void traverseStart(OntModel model, String entity, String rootQuery) {
+		// if starting class available
+		if (entity != null) {
+			traverse(model, entity, new ArrayList<String>(), 0);
+		}
+		// get roots and traverse each root
+		else {
+			List<String> roots = getRoots(model, rootQuery);
+
+			for (int i = 0; i < roots.size(); i++) {
+				traverse(model, roots.get(i), new ArrayList<String>(), 0);
+			}
+		}
+	}
+
+	public static void traverse(OntModel model, String entity, List<String> occurs, int depth) {
+		if (entity == null)
+			return;
+
+		String queryString = "SELECT ?s WHERE { " + "?s <http://www.w3.org/2000/01/rdf-schema#subClassOf> <" + entity
+				+ "> . }";
+
+		Query query = QueryFactory.create(queryString);
+
+		if (!occurs.contains(entity)) {
+			// print depth times "\t" to retrieve an explorer tree like output
+			for (int i = 0; i < depth; i++) {
+				System.out.print("\t");
+			}
+			// print out the URI
+			System.out.println(entity);
+
+			try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+				ResultSet results = qexec.execSelect();
+				while (results.hasNext()) {
+					QuerySolution soln = results.nextSolution();
+					RDFNode sub = soln.get("crime");
+					
+
+					if (!sub.isURIResource())
+						continue;
+
+					String str = sub.toString();
+
+					occurs.add(entity);
+					traverse(model, str, occurs, depth + 1);
+					occurs.remove(entity);
+				}
+
+			}
+		}
+
 	}
 
 }
